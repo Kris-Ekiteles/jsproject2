36a1b4436dde5089052d8cfb03cb176b9f4a4344
kris <krisekteles@gmail.com>
2025-07-04 12:52:36 +0300

    check prime number

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-const sentence = "My name is chris";
+//algorithm for character count
+const sentence = "My name is chris.";
 let sentenceLength = 0;
-alert(sentence.length);
+alert(`${sentence} the character count in sentence is, ${sentence.length}`);
+
+//algorithm for word count
+const string = "My name is kris, and i am loving javascript so far.";
+const word = string.split(" ");
+alert(`${string} the word count in sentence is, ${word.length}`);
+// mathematical functions
+// function for prime numbers
+// function prime() {
+//   const number = parseInt(prompt("enter a positive number"));
+//   let isPrime = true;
+//   if (number <= 1) {
+//     alert("please enter a value greater than 2");
+//   } else if (number > 1) {
+//     for (let i = 2; i <= number / 2; i++) {
+//       if (number % i == 0) {
+//         isPrime = false;
+//         alert(`{number} is a prime number`);
+//         break;
+//       }
+//     }
+//   } else {
+//     alert(`${number} is not a prime number`);
+//   }
+// }
+// function prime(){
+// const num= parseInt(prompt('enter a number'))
+
+// let i=2;
+// if (num<=1){
+//     alert('enter a number greater than 1')
+// }
+// else if(num>1)
+// }
+function prime() {
+  const num = parseInt(prompt("enter number"));
+  let i = 2;
+  if (num <= 1) {
+    alert("enter number greater that 2");
+  } else if (num >= 2 && num % i == 0) {
+    alert(`${number} is a prime`);
+  } else {
+    alert(`${number}is not a prime number`);
+  }
+}
